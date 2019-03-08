38559692a81fa79420ef02c089e14d2ce93d054a
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-03-08 11:02:05 +0100

    Added demo

---
+++ demo/js/helpers.js
@@ -0,0 +1,42 @@
+import { timeout } from "./oba-wrapper/js/modules/utils.js";
+
+export async function clearElement (element, interval = 10, reverse = false) {
+	while (element.childNodes.length) {
+		if (reverse) element.firstChild.remove();
+		else element.lastChild.remove();
+		await timeout(interval);
+	}
+}
+
+export function optionalChain (source, rest, alternative) {
+	//support obj[property] notation in rest
+	const chain = rest.split(/\./);
+	let obj = source[chain.shift()];
+	try {
+		for (const property of chain) obj = obj[property];
+		return (obj !== undefined)
+			? obj
+			: alternative;
+	} catch {
+		return alternative;
+	}
+}
+
+export function truncateString (string, length, pattern = "...") {
+	return (string.length > length)
+		? string
+			.slice(0, length)
+			.trim()
+			+ pattern
+		: string;
+}
+
+export function compareStrings (strA, strB) {
+	//Should make this smarter
+	return Math.abs(Math.min(strA.length, strB.length) - Math.max(strA.length, strB.length));
+}
+
+export function repeat (count, func) {
+	let i = 0;
+	while (i++ < count) func(i);
+}
---
+++ demo/js/render.js
@@ -0,0 +1,94 @@
+import { optionalChain, truncateString } from "./helpers.js";
+
+export function render (template) {
+	//Accepts a string with the following format:
+	//Starts with a tagname (\w+)
+	//followed by 1 or 0 #id (#\w+)
+	//followed by 1 or more .class (\.\w+)
+	//followed by 1 or 0 [...]
+	//	where ... = 1 or more attribute(=value)?
+	//	where every match is separated by a comma and 0 or more spaces
+	//followed by 1 or 0 "content"
+	// followed by 1 or more > ...
+	//	where ... = the entire pattern as described above.
+
+	function createElement (partial) {
+		const {
+			tagname = "div",
+			id,
+			classlist,
+			attributes,
+			content
+		} = partial
+			.trim()
+			.match(/^(?<tagname>\w+)(?<id>#\w+)?(?<classlist>\.\w+(?:\.\w+)*)*(?<attributes>\[(?:\w+(?:\s*=\s*.+)?)+(?:,\s*\w+(?:\s*=\s*\w+)?)*\])?\s*(?<content>"[^"]*")?/).groups;
+		const element = document.createElement(tagname);
+
+		if (id) element.id = id.substring(1);
+		if (classlist) element.classList.add(...classlist.split(/\./).filter(token => token !== ""));
+		if (attributes) attributes
+			.slice(1, -1)
+			.split(/,\s*/)
+			.forEach(attribute => {
+				const [key, value] = attribute.split(/=/);
+				element.setAttribute(key, value || "");
+			});
+		if (content) element.innerText = content.slice(1, -1);
+
+		return element;
+	}
+
+	const elements = template
+		.split(/\s*>\s*/)
+		.map(partial => createElement(partial));
+
+	elements
+		.reduce((parent, element) => {
+			parent.appendChild(element);
+			return element;
+		});
+
+	return elements[0];
+}
+
+export function resultsToDivs (results, {
+	titleLength = 48,
+	textLength = 144
+} = {}) {
+	return results.map(result => {
+		const title = truncateString(
+			optionalChain(
+				result,
+				"titles.short-title._text",
+				"Geen titel."),
+			titleLength);
+		const text = truncateString(
+			optionalChain(
+				result,
+				"summaries.summary._text",
+				"Geen samenvatting."),
+			textLength);
+		const div = render("div");
+		const img = render("img");
+		const h2 = render(`h2 "${title}"`);
+		const p = render(`p "${text}"`);
+
+		img.src = optionalChain( //Have to set src manually instead of in render() because of the following eventlistener
+			result,
+			"coverimages.coverimage.0._text")
+			|| optionalChain(
+				results,
+				"coverimages.coverimage._text",
+				"./src/images/coverimage_notfound.png");
+
+		img.addEventListener("load", () => { //Some images aren't actually images even though they load
+			if (img.width <= 1) img.src = optionalChain(
+				result,
+				"coverimages.coverimage.1._text",
+				"./src/images/coverimage_notfound.png");
+		});
+
+		div.append(img, h2, p);
+		return div;
+	});
+}
